25552c648003ccd1fdf8a9558a14bb801a073a12
Muhammad <ibneabdullatifjud@gmail.com>
2025-08-31 12:39:23 +0500

    create your own router

--- apps/widget/components/widget.tsx
+++ apps/widget/components/widget.tsx
@@ -3,14 +3,38 @@ import React from 'react'
 // import WidgetFooter from './widget-footer'
 // import WidgetHeader from './widget-header'
 import WidgetOnboardScreen from './screens/widget-onboard-screen'
+import { screenAtom } from '@/atoms/widget-atoms';
+import { useAtomValue } from 'jotai';
 interface Props {
     organizationId: string
 }
+type ScreenKey =
+  | 'error'
+  | 'loading'
+  | 'auth'
+  | 'voice'
+  | 'inbox'
+  | 'selection'
+  | 'chat'
+  | 'contact';
+
 function Widget({ organizationId }: Props) {
+  const screen = useAtomValue(screenAtom) as ScreenKey;
+  const screenComponents: Record<ScreenKey, React.ReactElement> = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetOnboardScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+  };
+
   return (
     //TODO: Confirm wether or not min-h and min-w is needed
     <main className='flex h-full min-h-screen min-w-screen w-full flex-col overflow-hidden rounded-xl border bg-muted text-xl'>
-       <WidgetOnboardScreen />
+       {screenComponents[screen]}
       {/* <WidgetFooter /> */}
     </main>
   )
