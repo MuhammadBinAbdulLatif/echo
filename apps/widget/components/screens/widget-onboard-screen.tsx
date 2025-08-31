7925e972811404538cfe2c1ddce9a43dda55e17d
Muhammad <ibneabdullatifjud@gmail.com>
2025-08-31 12:02:31 +0500

    onboard the user for chat

---
+++ apps/widget/components/screens/widget-onboard-screen.tsx
@@ -0,0 +1,140 @@
+"use client";
+import React from "react";
+import WidgetHeader from "../widget-header";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
+import { Doc } from "@workspace/backend/convex/_generated/dataModel";
+import { Loader2 } from "lucide-react";
+
+const WidgetOnboardScreen = () => {
+  // ✅ Add validation rules
+  const formSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email").min(1, "Email is required"),
+  });
+
+  const organizationId = "64a7f0f4c9e77b001f8b4567"; // TODO: replace after state management
+  const createContactSession = useMutation(api.public.contactSessions.create);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!organizationId) return;
+
+    const metadata: Doc<"contactSessions">["metadata"] = {
+      userAgent: navigator.userAgent,
+      language: navigator.language,
+      languages: navigator.languages?.join(","),
+      platform: navigator.platform,
+      vendor: navigator.vendor,
+      screenResolution: `${window.screen.width}x${window.screen.height}`,
+      viewPortSize: `${window.innerWidth}x${window.innerHeight}`,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      timezoneOffset: new Date().getTimezoneOffset(),
+      cookieEnabled: navigator.cookieEnabled,
+      referrer: document.referrer || "direct",
+      currentUrl: window.location.href,
+    };
+
+    const contactSessionId = await createContactSession({
+      ...values,
+      organizationId,
+      metadata,
+    });
+
+    console.log({ contactSessionId });
+  }
+
+  function onReset() {
+    form.reset();
+    form.clearErrors();
+  }
+
+  return (
+    <>
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+          <p className="text-3xl">Hi there 🤚</p>
+          <p className="text-lg">Let's get you started!</p>
+        </div>
+      </WidgetHeader>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          onReset={onReset}
+          className="space-y-8 @container"
+        >
+          <div className="flex flex-col py-2 px-2 gap-4">
+            {/* Name */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2 items-start">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      key="name"
+                      placeholder="John Doe"
+                      type="text"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2 items-start">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      key="email"
+                      placeholder="johndoe@gmail.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Button with loader */}
+            <Button type="submit" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting ? <Loader2 className="size-8 animate-spin" /> : "Continue"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default WidgetOnboardScreen;
--- apps/widget/components/widget.tsx
+++ apps/widget/components/widget.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React from 'react'
-import WidgetFooter from './widget-footer'
-import WidgetHeader from './widget-header'
+// import WidgetFooter from './widget-footer'
+// import WidgetHeader from './widget-header'
+import WidgetOnboardScreen from './screens/widget-onboard-screen'
 interface Props {
     organizationId: string
 }
@@ -9,20 +10,8 @@ function Widget({ organizationId }: Props) {
   return (
     //TODO: Confirm wether or not min-h and min-w is needed
     <main className='flex h-full min-h-screen min-w-screen w-full flex-col overflow-hidden rounded-xl border bg-muted text-xl'>
-        <WidgetHeader>
-            <div className='flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold'>
-                <p className='text-3xl'>
-                    Hi there 🤚
-                </p>
-                <p className='text-lg'>
-                    How can we help you today?
-                </p>
-            </div>
-        </WidgetHeader>
-       <div className='flex flex-1 '>
-         Widget View {organizationId}
-       </div>
-      <WidgetFooter />
+       <WidgetOnboardScreen />
+      {/* <WidgetFooter /> */}
     </main>
   )
 }
